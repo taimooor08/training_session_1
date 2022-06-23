dbbb7778ec868083ad62cb65f883ac49e41b1ffa
taimooor08 <takhan@nisum.com>
2022-06-23 12:30:43 +0500

    First Commit for training

---
+++ src/test/java/MyClassOne.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MyClassOne {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println ("Enter your name:");
+        String name = sc.nextLine();
+
+        System.out.println("My name is " + name);
+
+    }
+}
